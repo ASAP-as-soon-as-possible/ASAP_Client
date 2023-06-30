4e7727acc75a48ab598a0aa09757ec6fd1f88814
ljh0608 <m98m@catholic.ac.kr>
2023-07-01 01:16:37 +0900

    init: eslint 추가수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-
-import './App.css';
+import Test from './pages/Test';
 
 function App() {
   return (
     <>
       <h1>하이!</h1>
+      <Test></Test>
     </>
   );
 }
