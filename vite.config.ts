8a1e6db0795e66683bbb5c29fed6c778edb70856
ljh0608 <m98m@catholic.ac.kr>
2023-07-01 11:38:07 +0900

    init: styled-components/macro settings

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import path from 'path';
 
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-
+import macrosPlugin from 'vite-plugin-babel-macros';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), macrosPlugin()],
+
   resolve: {
     alias: [
       { find: '/', replacement: path.resolve(__dirname, '/') },
