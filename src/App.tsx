29d3dc2b6528eb8c6c8a93a7282eec20fddce529
ljh0608 <m98m@catholic.ac.kr>
2023-07-03 17:08:17 +0900

    init: globalStyle적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,57 @@
-import TestComponents from 'components/TestComponents';
-import Test from 'pages/Test';
+import { useState } from 'react';
+
+import ScrollerComponent from 'pages/ScrollerComponent';
+import styled from 'styled-components/macro';
+
+import GlobalStyle from './styles/globalStyles';
 
 function App() {
+  const [selectedHour, setSelectedHour] = useState(1);
+  const [selectedMinute, setSelectedMinute] = useState(1);
+  const [selectedSecond, setSelectedSecond] = useState(1);
+
   return (
     <>
-      <h1>하이!</h1>
-      <Test></Test>
-      <TestComponents></TestComponents>
+      <GlobalStyle />
+      <MainContainer>
+        <ScrollerComponent
+          min={0}
+          max={24}
+          value={selectedHour}
+          setValue={setSelectedHour}
+          title="Hour"
+        />
+        <ColonSpan>:</ColonSpan>
+        <ScrollerComponent
+          min={0}
+          max={59}
+          value={selectedMinute}
+          setValue={setSelectedMinute}
+          title="Minute"
+        />
+        <ColonSpan>:</ColonSpan>
+        <ScrollerComponent
+          min={0}
+          max={59}
+          value={selectedSecond}
+          setValue={setSelectedSecond}
+          title="Second"
+        />
+      </MainContainer>
     </>
   );
 }
 
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin: calc(50vh - 100px) 0px;
+  width: 100vw;
+  height: 200px;
+`;
+const ColonSpan = styled.span`
+  margin: 150px 0px 100px;
+`;
 export default App;
--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,1 +1,29 @@
 //
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+
+body {
+  overscroll-behavior : contain;
+margin: 0 auto;
+}
+
+a {
+cursor: pointer;
+text-decoration: none;
+color:inherit;
+}
+
+button {
+border: none;
+background: none;
+cursor: pointer;
+font: inherit;
+
+}
+`;
+
+export default GlobalStyle;
