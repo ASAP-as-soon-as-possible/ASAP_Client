1f87c4845e31315f1af81bac57620ae458136be3
ljh0608 <m98m@catholic.ac.kr>
2023-07-06 05:36:35 +0900

    feat: mobile view 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,47 @@
+import { useEffect } from 'react';
+
 import { ThemeProvider } from 'styled-components';
+import styled from 'styled-components/macro';
 
 import Router from './Router';
 import GlobalStyle from './styles/globalStyles';
 import { theme } from './styles/theme';
 
-//test
+const MobileWrapper = styled.div`
+  position: relative;
+  margin-right: auto;
+  margin-left: auto;
+
+  max-width: var(--app-max-width, 37.5rem);
+  height: 100vh;
+  min-height: calc(var(--vh, 1vh) * 100);
+`;
+
 function App() {
+  const setScreenSize = () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+    const windowWidth = window.innerWidth || document.documentElement.clientWidth;
+    const maxWidth = Math.min(37.5, windowWidth);
+    document.documentElement.style.setProperty('--app-max-width', `${maxWidth}rem`);
+  };
+
+  useEffect(() => {
+    setScreenSize();
+    window.addEventListener('resize', setScreenSize);
+
+    return () => {
+      window.removeEventListener('resize', setScreenSize);
+    };
+  }, []);
   return (
     <>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <Router />
+        <MobileWrapper>
+          <Router />
+        </MobileWrapper>
       </ThemeProvider>
     </>
   );
