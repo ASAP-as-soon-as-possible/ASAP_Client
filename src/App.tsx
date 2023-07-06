50b146e63bed1286cffb2055fafeb3abeae5b3d3
ljh0608 <m98m@catholic.ac.kr>
2023-07-07 01:53:07 +0900

    design: mobile뷰 배경적용

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const MobileWrapper = styled.div`
   position: relative;
   margin-right: auto;
   margin-left: auto;
-  background-color: black;
+  background-color: ${({ theme }) => theme.colors.black};
 
   max-width: var(--app-max-width, 37.5rem);
 
