6d3f3d68738d0a65faaba667db1f5302ce1f5847
ljh0608 <m98m@catholic.ac.kr>
2023-07-06 12:10:36 +0900

    style: css 포맷팅 순서 변경

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -5,26 +5,29 @@ const GlobalStyle = createGlobalStyle`
   ${reset}
 
 * {
-  font-family: "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
   box-sizing: border-box;
-}
+  font-family: "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
 
+}
+/* overscroll-behavior : contain; */ //모바일 아래 드래그로 새로고침 막는 코드
   html, body {
-  /* overscroll-behavior : contain; */ //모바일 아래 드래그로 새로고침 막는 코드
-margin: 0 auto;
+  margin: 0 auto;
   font-size: 62.5%;
+
 }
 
 a {
 cursor: pointer;
+
 text-decoration: none;
 color:inherit;
 }
 
+
 button {
 border: none;
 background: none;
-cursor: pointer;
+  cursor: pointer;
 font: inherit;
 
 }
