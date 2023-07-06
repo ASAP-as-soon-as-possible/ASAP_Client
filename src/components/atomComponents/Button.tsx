5ff271fb44013ee00216378960d66ebdd1bed792
Chanwoo-Jeong <lordroad38@naver.com>
2023-07-07 00:17:20 +0900

    feat: button component 마무리

--- src/components/atomComponents/Button.tsx
+++ src/components/atomComponents/Button.tsx
@@ -1,15 +1,16 @@
+
 import React from 'react';
 
 import { css, styled } from 'styled-components'
 
 
 interface ButtonProps {
-    children: React.ReactNode,
-    typeState: string
-    onClick?:()=>void
+  children: React.ReactNode,
+  typeState: string
+  onClick?: () => void
 }
 
-function Button({ children, typeState ,onClick }: ButtonProps) {
+function Button({ children, typeState, onClick }: ButtonProps) {
   return (
     <StyledBtn type={typeState} onClick={onClick}>{children}</StyledBtn>
   )
@@ -18,14 +19,14 @@ function Button({ children, typeState ,onClick }: ButtonProps) {
 export default Button
 
 const buttonDefaultCSS = {
-  basicCss : css`
+  basicCss: css`
   display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 0.8rem;
+  padding: 1.6rem;
   width: 30.3rem;
   height: 5.4rem;
-  padding: 1.6rem;
-  justify-content: center;
-  align-items: center;
-  border-radius: 0.8rem;
   letter-spacing: -0.032rem;
   `
 }
@@ -33,73 +34,73 @@ const buttonDefaultCSS = {
 const buttonCSS = {
   primaryActive: css`
       ${buttonDefaultCSS.basicCss};
-      background: ${({theme})=>theme.colors.main1};
+      background: ${({ theme }) => theme.colors.main1};
       color: #FFFFFF;
     `,
   halfPrimaryActive: css`
-  ${buttonDefaultCSS.basicCss};
-  width:16rem;
-  background: ${({theme})=>theme.colors.main1};
-  color: #FFFFFF;
+      ${buttonDefaultCSS.basicCss};
+      background: ${({ theme }) => theme.colors.main1};
+      width:16rem;
+      color: #FFFFFF;
     `,
   primaryDisabled: css`
       ${buttonDefaultCSS.basicCss};
-      background: ${({theme})=>theme.colors.grey7};
-      color: ${({theme})=>theme.colors.white};
+      background: ${({ theme }) => theme.colors.grey7};
+      color: ${({ theme }) => theme.colors.white};
     `,
   secondaryActive: css`
       ${buttonDefaultCSS.basicCss};
-      background: ${({theme})=>theme.colors.main5};
-      color:  ${({theme})=>theme.colors.grey9};
+      background: ${({ theme }) => theme.colors.main5};
+      color:  ${({ theme }) => theme.colors.grey9};
     `,
   secondaryDisabled: css`
       ${buttonDefaultCSS.basicCss};
-      background: ${({theme})=>theme.colors.grey7};
-      color: ${({theme})=>theme.colors.grey5};
+      background: ${({ theme }) => theme.colors.grey7};
+      color: ${({ theme }) => theme.colors.grey5};
     `,
   tertiaryActive: css`
-    ${buttonDefaultCSS.basicCss};
-    background-color: transparent;
-    border: 1px solid ${({theme})=>theme.colors.main3};
-    color: ${({theme})=>theme.colors.main3};
+      ${buttonDefaultCSS.basicCss};
+      border: 1px solid ${({ theme }) => theme.colors.main3};
+      background-color: transparent;
+      color: ${({ theme }) => theme.colors.main3};
 `,
   halfTertiaryActive: css`
-    ${buttonDefaultCSS.basicCss};
-    width: 16rem;
-    background-color: transparent;
-    border: 1px solid ${({theme})=>theme.colors.main3};
-    color: ${({theme})=>theme.colors.main3};
+      ${buttonDefaultCSS.basicCss};
+      border: 1px solid ${({ theme }) => theme.colors.main3};
+      background-color: transparent;
+      width: 16rem;
+      color: ${({ theme }) => theme.colors.main3};
 `,
   tertiaryDisabled: css`
-    ${buttonDefaultCSS.basicCss};
-    background-color: transparent;
-    border: 1px solid ${({theme})=>theme.colors.grey6};
-    color: ${({theme})=>theme.colors.grey6};
+      ${buttonDefaultCSS.basicCss};
+      border: 1px solid ${({ theme }) => theme.colors.grey6};
+      background-color: transparent;
+      color: ${({ theme }) => theme.colors.grey6};
 `,
 };
 
 const StyledBtn = styled.button<{ type: string }>`
-${({ type }) => {
+  ${({ type }) => {
     switch (type) {
-    case "primaryActive":
-      return buttonCSS.primaryActive;
-    case "halfPrimaryActive":
-      return buttonCSS.halfPrimaryActive;
-    case "primaryDisabled":
-      return buttonCSS.primaryDisabled;
-    case "secondaryActive":
-      return buttonCSS.secondaryActive;
-    case "secondaryDisabled":
-      return buttonCSS.secondaryDisabled;
-    case "tertiaryActive":
-      return buttonCSS.tertiaryActive;
-    case "halfTertiaryActive":
-      return buttonCSS.halfTertiaryActive;
-    case "tertiaryDisabled":
-      return buttonCSS.tertiaryDisabled;
-    default:
-      return '';
+      case "primaryActive":
+        return buttonCSS.primaryActive;
+      case "halfPrimaryActive":
+        return buttonCSS.halfPrimaryActive;
+      case "primaryDisabled":
+        return buttonCSS.primaryDisabled;
+      case "secondaryActive":
+        return buttonCSS.secondaryActive;
+      case "secondaryDisabled":
+        return buttonCSS.secondaryDisabled;
+      case "tertiaryActive":
+        return buttonCSS.tertiaryActive;
+      case "halfTertiaryActive":
+        return buttonCSS.halfTertiaryActive;
+      case "tertiaryDisabled":
+        return buttonCSS.tertiaryDisabled;
+      default:
+        return '';
     }
   }
-}
+  }
 `
