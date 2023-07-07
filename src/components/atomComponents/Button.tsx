a5e8b056abb7b92d899ed472ac8a2728dc39a4de
Chanwoo-Jeong <lordroad38@naver.com>
2023-07-08 02:23:41 +0900

    refactor: transient prop 적용

--- src/components/atomComponents/Button.tsx
+++ src/components/atomComponents/Button.tsx
@@ -1,106 +1,105 @@
-
 import React from 'react';
 
-import { css, styled } from 'styled-components'
-
+import { css, styled } from 'styled-components';
 
 interface ButtonProps {
-  children: React.ReactNode,
-  typeState: string
-  onClick?: () => void
+  children: React.ReactNode;
+  typeState: string;
+  onClick?: () => void;
 }
 
 function Button({ children, typeState, onClick }: ButtonProps) {
   return (
-    <StyledBtn type={typeState} onClick={onClick}>{children}</StyledBtn>
-  )
+    <StyledBtn $type={typeState} onClick={onClick}>
+      {children}
+    </StyledBtn>
+  );
 }
 
-export default Button
+export default Button;
 
 const buttonDefaultCSS = {
   basicCss: css`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 0.8rem;
-  padding: 1.6rem;
-  width: 30.3rem;
-  height: 5.4rem;
-  letter-spacing: -0.032rem;
-  `
-}
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 0.8rem;
+    padding: 1.6rem;
+    width: 30.3rem;
+    height: 5.4rem;
+    letter-spacing: -0.032rem;
+  `,
+};
 
 const buttonCSS = {
   primaryActive: css`
-      ${buttonDefaultCSS.basicCss};
-      background: ${({ theme }) => theme.colors.main1};
-      color: ${({ theme }) => theme.colors.white};
-    `,
+    ${buttonDefaultCSS.basicCss};
+    background: ${({ theme }) => theme.colors.main1};
+    color: ${({ theme }) => theme.colors.white};
+  `,
   halfPrimaryActive: css`
-      ${buttonDefaultCSS.basicCss};
-      background: ${({ theme }) => theme.colors.main1};
-      width:16rem;
-      color: ${({ theme }) => theme.colors.white};
-    `,
+    ${buttonDefaultCSS.basicCss};
+    background: ${({ theme }) => theme.colors.main1};
+    width: 16rem;
+    color: ${({ theme }) => theme.colors.white};
+  `,
   primaryDisabled: css`
-      ${buttonDefaultCSS.basicCss};
-      background: ${({ theme }) => theme.colors.grey7};
-      color: ${({ theme }) => theme.colors.white};
-    `,
+    ${buttonDefaultCSS.basicCss};
+    background: ${({ theme }) => theme.colors.grey7};
+    color: ${({ theme }) => theme.colors.white};
+  `,
   secondaryActive: css`
-      ${buttonDefaultCSS.basicCss};
-      background: ${({ theme }) => theme.colors.main5};
-      color:  ${({ theme }) => theme.colors.grey9};
-    `,
+    ${buttonDefaultCSS.basicCss};
+    background: ${({ theme }) => theme.colors.main5};
+    color: ${({ theme }) => theme.colors.grey9};
+  `,
   secondaryDisabled: css`
-      ${buttonDefaultCSS.basicCss};
-      background: ${({ theme }) => theme.colors.grey7};
-      color: ${({ theme }) => theme.colors.grey5};
-    `,
+    ${buttonDefaultCSS.basicCss};
+    background: ${({ theme }) => theme.colors.grey7};
+    color: ${({ theme }) => theme.colors.grey5};
+  `,
   tertiaryActive: css`
-      ${buttonDefaultCSS.basicCss};
-      border: 1px solid ${({ theme }) => theme.colors.main3};
-      background-color: transparent;
-      color: ${({ theme }) => theme.colors.main3};
-`,
+    ${buttonDefaultCSS.basicCss};
+    border: 1px solid ${({ theme }) => theme.colors.main3};
+    background-color: transparent;
+    color: ${({ theme }) => theme.colors.main3};
+  `,
   halfTertiaryActive: css`
-      ${buttonDefaultCSS.basicCss};
-      border: 1px solid ${({ theme }) => theme.colors.main3};
-      background-color: transparent;
-      width: 16rem;
-      color: ${({ theme }) => theme.colors.main3};
-`,
+    ${buttonDefaultCSS.basicCss};
+    border: 1px solid ${({ theme }) => theme.colors.main3};
+    background-color: transparent;
+    width: 16rem;
+    color: ${({ theme }) => theme.colors.main3};
+  `,
   tertiaryDisabled: css`
-      ${buttonDefaultCSS.basicCss};
-      border: 1px solid ${({ theme }) => theme.colors.grey6};
-      background-color: transparent;
-      color: ${({ theme }) => theme.colors.grey6};
-`,
+    ${buttonDefaultCSS.basicCss};
+    border: 1px solid ${({ theme }) => theme.colors.grey6};
+    background-color: transparent;
+    color: ${({ theme }) => theme.colors.grey6};
+  `,
 };
 
-const StyledBtn = styled.button<{ type: string }>`
-  ${({ type }) => {
-    switch (type) {
-      case "primaryActive":
+const StyledBtn = styled.button<{ $type: string }>`
+  ${({ $type }) => {
+    switch ($type) {
+      case 'primaryActive':
         return buttonCSS.primaryActive;
-      case "halfPrimaryActive":
+      case 'halfPrimaryActive':
         return buttonCSS.halfPrimaryActive;
-      case "primaryDisabled":
+      case 'primaryDisabled':
         return buttonCSS.primaryDisabled;
-      case "secondaryActive":
+      case 'secondaryActive':
         return buttonCSS.secondaryActive;
-      case "secondaryDisabled":
+      case 'secondaryDisabled':
         return buttonCSS.secondaryDisabled;
-      case "tertiaryActive":
+      case 'tertiaryActive':
         return buttonCSS.tertiaryActive;
-      case "halfTertiaryActive":
+      case 'halfTertiaryActive':
         return buttonCSS.halfTertiaryActive;
-      case "tertiaryDisabled":
+      case 'tertiaryDisabled':
         return buttonCSS.tertiaryDisabled;
       default:
         return '';
     }
-  }
-  }
-`
+  }};
+`;
