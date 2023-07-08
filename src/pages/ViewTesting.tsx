17c19871fb94e9bf437d96ce5b251572f9ae81bc
Chanwoo-Jeong <lordroad38@naver.com>
2023-07-09 02:53:02 +0900

    fix: 버튼 type 변경

--- src/pages/ViewTesting.tsx
+++ src/pages/ViewTesting.tsx
@@ -3,15 +3,16 @@ import { useState } from 'react';
 import Button from 'components/atomComponents/Button';
 import Text from 'components/atomComponents/Text';
 import TextInput from 'components/atomComponents/TextInput';
+import Header from 'components/moleculesComponents/Header';
 import TitleComponents from 'components/moleculesComponents/TitleComponent';
 import { styled } from 'styled-components';
-import { theme } from 'styles/theme';
 
 function ViewTesting() {
   const [inputValue, setInputValue] = useState(``);
   return (
     <>
       <ViewTestingWrapper>
+        <Header />
         <TitleComponents
           main={'어떤 회의를 계획중인가요?'}
           sub={'회의 이름을 지어주세요 (최대 15자)'}
@@ -23,12 +24,12 @@ function ViewTesting() {
         />
         <StyledBtnWrapper>
           <Button
-            typeState={inputValue && inputValue.length < 16 ? 'primaryActive' : 'primaryDisabled'}
+            typeState={inputValue && inputValue.length < 16 ? 'primaryActive' : 'secondaryDisabled'}
             onClick={
               inputValue && inputValue.length < 16 ? () => console.log('im clickable') : undefined
             }
           >
-            <Text font={'button2'} color={`${theme.colors.black}`}>
+            <Text font={'button2'} >
               약속 생성하기
             </Text>
           </Button>
