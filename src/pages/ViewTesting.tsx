11a3452d475b658b799871f9a9758640ae4b3b1a
Chanwoo-Jeong <lordroad38@naver.com>
2023-07-09 01:28:28 +0900

    feat: ViewTesting Component 채워보기 test

--- src/pages/ViewTesting.tsx
+++ src/pages/ViewTesting.tsx
@@ -1,9 +1,17 @@
+import { useState } from 'react';
+
+import TextInput from 'components/atomComponents/TextInput';
 import TitleComponents from 'components/moleculesComponents/TitleComponents';
 
 function ViewTesting() {
+  const [inputValue, setInputValue] = useState(``);
   return (
     <>
-      <TitleComponents />
+      <TitleComponents
+        main={'어떤 회의를 계획중인가요?'}
+        sub={'회의 이름을 지어주세요 (최대 15자)'}
+      />
+      <TextInput value={inputValue} setValue={setInputValue} placeholder={'서비스 기획 1차 회의'} />
     </>
   );
 }
