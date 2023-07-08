42989b59671f3670066ec92e4c39ee406aa04a5d
Chanwoo-Jeong <lordroad38@naver.com>
2023-07-09 02:11:43 +0900

    feat: 회의명 입력 페이지 testCode

--- src/pages/ViewTesting.tsx
+++ src/pages/ViewTesting.tsx
@@ -1,19 +1,53 @@
 import { useState } from 'react';
 
+import Button from 'components/atomComponents/Button';
+import Text from 'components/atomComponents/Text';
 import TextInput from 'components/atomComponents/TextInput';
 import TitleComponents from 'components/moleculesComponents/TitleComponents';
+import { styled } from 'styled-components';
+import { theme } from 'styles/theme';
 
 function ViewTesting() {
   const [inputValue, setInputValue] = useState(``);
   return (
     <>
-      <TitleComponents
-        main={'어떤 회의를 계획중인가요?'}
-        sub={'회의 이름을 지어주세요 (최대 15자)'}
-      />
-      <TextInput value={inputValue} setValue={setInputValue} placeholder={'서비스 기획 1차 회의'} />
+      <ViewTestingWrapper>
+        <TitleComponents
+          main={'어떤 회의를 계획중인가요?'}
+          sub={'회의 이름을 지어주세요 (최대 15자)'}
+        />
+        <TextInput
+          value={inputValue}
+          setValue={setInputValue}
+          placeholder={'서비스 기획 1차 회의'}
+        />
+        <StyledBtnWrapper>
+          <Button
+            typeState={inputValue && inputValue.length < 16 ? 'primaryActive' : 'primaryDisabled'}
+            onClick={
+              inputValue && inputValue.length < 16 ? () => console.log('im clickable') : undefined
+            }
+          >
+            <Text font={'button2'} color={`${theme.colors.black}`}>
+              약속 생성하기
+            </Text>
+          </Button>
+        </StyledBtnWrapper>
+      </ViewTestingWrapper>
     </>
   );
 }
 
 export default ViewTesting;
+
+const ViewTestingWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+const StyledBtnWrapper = styled.section`
+  position: fixed;
+  bottom: 1.2rem;
+  border-radius: 50%;
+`;
