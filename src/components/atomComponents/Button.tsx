3742c4b4efed3092fb31ce3183f0df07e724020e
Chanwoo-Jeong <lordroad38@naver.com>
2023-07-09 02:11:11 +0900

    design: Button width 값 수정

--- src/components/atomComponents/Button.tsx
+++ src/components/atomComponents/Button.tsx
@@ -25,7 +25,7 @@ const buttonDefaultCSS = {
     justify-content: center;
     border-radius: 0.8rem;
     padding: 1.6rem;
-    width: 30.3rem;
+    width: 33.5rem;
     height: 5.4rem;
     letter-spacing: -0.032rem;
   `,
