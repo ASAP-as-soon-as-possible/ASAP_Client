9348e45b2be1d956eb9562c308a035e8438291ac
Chanwoo-Jeong <lordroad38@naver.com>
2023-07-09 00:46:15 +0900

    feat: viewTesting Router 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,5 @@
 import ComponentTesting from 'pages/ComponentTesting';
+import ViewTesting from 'pages/ViewTesting';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const Router = () => {
@@ -6,6 +7,7 @@ const Router = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/componenttest" element={<ComponentTesting />} />
+        <Route path="/viewtesting" element={<ViewTesting />} />
       </Routes>
     </BrowserRouter>
   );
