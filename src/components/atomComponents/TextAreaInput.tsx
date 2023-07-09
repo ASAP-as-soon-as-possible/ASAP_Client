4f02c034ddb3b35406fbdbd74ef03519b013e3c1
Chanwoo-Jeong <lordroad38@naver.com>
2023-07-09 17:47:43 +0900

    design : textAreaInput Component 제작

---
+++ src/components/atomComponents/TextAreaInput.tsx
@@ -0,0 +1,53 @@
+import React, { Dispatch, SetStateAction } from 'react';
+
+import Text from 'components/atomComponents/Text';
+import styled from 'styled-components/macro';
+import { theme } from 'styles/theme';
+
+interface ValueProps {
+  value: string;
+  setValue: Dispatch<SetStateAction<string>>;
+  placeholder: string;
+}
+
+function TextAreaInput({ value, setValue, placeholder }: ValueProps) {
+  const textOnChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setValue(e.currentTarget.value);
+  };
+
+  return (
+    <MultiLineInputWrapper>
+      <StyledTextArea placeholder={placeholder} value={value} onChange={textOnChange} />
+      <CountingLetterSection>
+        <Text font={'body3'} color={`${theme.colors.grey5}`} >{`(${value?.length} / 50자)`}</Text>
+      </CountingLetterSection>
+    </MultiLineInputWrapper>
+  );
+}
+
+export default TextAreaInput;
+
+const MultiLineInputWrapper = styled.div`
+  position:relative;
+`;
+
+const StyledTextArea = styled.textarea`
+  border: none;
+  border-radius: 1rem;
+  background-color: ${({ theme }) => theme.colors.grey8};
+  padding: 1.8rem;
+  width: 33.5rem;
+  height: 20.8rem;
+  resize: none;
+  color: ${({ theme }) => theme.colors.white};
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+const CountingLetterSection = styled.section`
+  position:absolute;
+  right:1.8rem;
+  bottom:1.8rem;
+`;
