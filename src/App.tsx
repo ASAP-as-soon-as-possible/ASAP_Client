3da2289925e8714e1572eaa4900bcb8f527bb714
ljh0608 <m98m@catholic.ac.kr>
2023-07-12 16:41:43 +0900

    design: app 내부 black 적용

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,12 @@ const MobileWrapper = styled.div`
   position: relative;
   margin-right: auto;
   margin-left: auto;
-  /* background-color: ${({ theme }) => theme.colors.black}; */
+  background-color: ${({ theme }) => theme.colors.black};
   padding-right: 2rem;
   padding-left: 2rem;
 
   max-width: var(--app-max-width, 37.5rem);
   min-height: calc(var(--vh, 1vh) * 100);
-
 `;
 
 function App() {
