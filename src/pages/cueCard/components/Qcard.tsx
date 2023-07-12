6f82828b3320ead05f3a7f24a5dd35852ac4d638
Chanwoo-Jeong <lordroad38@naver.com>
2023-07-13 01:07:44 +0900

    design: Top CueCard Design 제작

--- src/pages/cueCard/components/Qcard.tsx
+++ src/pages/cueCard/components/Qcard.tsx
@@ -1,4 +1,5 @@
 import Text from 'components/atomComponents/Text';
+import { OnlinePlaceIc, TimeIc } from 'components/Icon/icon';
 import styled from 'styled-components';
 import { theme } from 'styles/theme';
 
@@ -8,13 +9,39 @@ function Qcard() {
       <TopCardSetcion>
         <Text font={'head2'}>아삽 전체회의</Text>
         <PlaceTimeSection>
-            <PlaceContainer>
-                <Text font={'title2'}>신촌 마카닷 스터카페</Text>
-            </PlaceContainer>
-            <TimeContainer>
-                <Text font={'title2'}>6월 30 (금) 18:00-21:00</Text>
-            </TimeContainer>
+          <PlaceContainer>
+            <IconBox>
+              <OnlinePlaceIc />
+            </IconBox>
+            <Text font={'title2'}>신촌 마카닷 스터카페</Text>
+          </PlaceContainer>
+          <TimeContainer>
+            <IconBox>
+              <TimeIc />
+            </IconBox>
+            <Text font={'title2'}>6월 30 (금) 18:00-21:00</Text>
+          </TimeContainer>
         </PlaceTimeSection>
+        <MemeberSection>
+          <HostMeberContainer>
+            <MemberTitle>
+            <Text font={'body2'} color={`${theme.colors.main1}`}>
+              방장
+            </Text>
+            </MemberTitle>
+            <Text font={'body2'}>서지원</Text>
+          </HostMeberContainer>
+          <MemberContainer>
+            <MemberTitle>
+            <Text font={'body2'} color={`${theme.colors.main1}`}>
+              참여
+            </Text>
+            </MemberTitle>
+            <Text font={'body2'}>
+              서지원, 서채원, 강민서, 서지원, 서지원, 서채원, 강민서, 서지원
+            </Text>
+          </MemberContainer>
+        </MemeberSection>
       </TopCardSetcion>
     </QcardWrapper>
   );
@@ -28,6 +55,9 @@ const QcardWrapper = styled.article`
 `;
 
 const TopCardSetcion = styled.section`
+  display:flex;
+  flex-direction: column;
+  gap:3.2rem;
   margin: 0 2rem;
   border: 2px solid ${theme.colors.white};
   border-top-left-radius: 1rem;
@@ -38,12 +68,43 @@ const TopCardSetcion = styled.section`
   height: 30rem;
 `;
 
-const PlaceTimeSection =styled.section`
-  margin-top: 3.3rem;
-`
-const PlaceContainer =styled.section`
+const PlaceTimeSection = styled.section`
+`;
+const PlaceContainer = styled.section`
+  display: flex;
+  flex-direction: row;
+  gap: 2rem;
+`;
+const TimeContainer = styled.section`
+  display: flex;
+  flex-direction: row;
+  gap: 2rem;
+`;
 
-`
-const TimeContainer =styled.section`
+const IconBox = styled.span`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 2rem;
+  height: 2.4rem;
+`;
 
+const MemeberSection = styled.section`
+  display: flex;
+  flex-direction: column;
+`;
+const HostMeberContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap:1.5rem;
+`;
+
+const MemberContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap:1.5rem;
+`;
+
+const MemberTitle = styled.div`
+  min-width: 2.5rem;
 `
