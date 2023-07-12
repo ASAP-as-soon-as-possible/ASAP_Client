1d7d70daffa602447fd33b040d998ce16c174935
Chanwoo-Jeong <lordroad38@naver.com>
2023-07-12 17:14:57 +0900

    fix: undefined 제거

--- src/pages/createMeeting/types/useFunnelInterface.ts
+++ src/pages/createMeeting/types/useFunnelInterface.ts
@@ -1,19 +1,18 @@
 import { Dispatch, SetStateAction } from 'react';
 
 export interface MeetingInfo {
-    title: string;
-    availableDates: string[];
-    preferTimes: {
-      startTime: string;
-      endTime: string;
-    }[];
-    //undefined 왜 들어간거임?
-    place: string | undefined;
-    placeDetail: string;
-    duration: string;
-    name: string;
-    password: string;
-    additionalInfo: string;
+  title: string;
+  availableDates: string[];
+  preferTimes: {
+    startTime: string;
+    endTime: string;
+  }[];
+  place: string;
+  placeDetail: string;
+  duration: string;
+  name: string;
+  password: string;
+  additionalInfo: string;
 }
 
 export interface funnelProps {
