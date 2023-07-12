54c7670c188330ab92d4c46ce2dca60f56be262a
Chanwoo-Jeong <lordroad38@naver.com>
2023-07-13 04:11:52 +0900

    design: CueCardTitle 생성 및 적용

--- src/pages/cueCard/CueCard.tsx
+++ src/pages/cueCard/CueCard.tsx
@@ -5,12 +5,13 @@ import Header from 'components/moleculesComponents/Header';
 import TitleComponent from 'components/moleculesComponents/TitleComponent';
 import { theme } from 'styles/theme';
 import Qcard from './components/Qcard';
+import CueCardTitle from './components/cueCardTitle';
 
 function CueCard() {
   return (
     <>
       <Header />
-      <TitleComponent main={'회의 일정이 확정되었어요!'} sub={'이미지를 저장하거나 링크를 복사하여 팀원에게 공유해보세요'} />
+      <CueCardTitle main={'일정 조율 완료'} sub={'이미 확정된 회의 일정입니다'} />
       <Qcard />
     </>
   );
