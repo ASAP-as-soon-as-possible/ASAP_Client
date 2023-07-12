8518a6c74ac62eaad39e2fc6edeadfae4645c7f8
ljh0608 <m98m@catholic.ac.kr>
2023-07-12 15:54:58 +0900

    fix: url params

--- src/Router.tsx
+++ src/Router.tsx
@@ -8,15 +8,12 @@ const Router = () => {
         <Route path="/componenttest" element={<ComponentTesting />} />
         <Route path="/" element={<div>온보딩 페이지를 넣어주세요</div>} />
         <Route path="/meet/create" element={<div>회의 정보 입력페이지를 넣어주세요</div>} />
-        <Route
-          path="/meet/complete/:meetingId"
-          element={<div>회의 생성 완료페이지를 넣어주세요</div>}
-        />
+        <Route path="/meet/complete" element={<div>회의 생성 완료페이지를 넣어주세요</div>} />
         <Route path="/schedule/:meetingId" element={<div>가능 시간 입력페이지를 넣어주세요</div>} />
         <Route path="/priority/:meetingId" element={<div>우선순위 선정페이지를 넣어주세요</div>} />
         <Route path="/schedule-complete/:meetingId" element={<div>일정 입력 완료 페이지</div>} />
         <Route path="/:meetingId" element={<div>링크입장페이지</div>} />
-        <Route path="/login" element={<div>로그인 페이지를 넣어주세요</div>} />
+        <Route path="/login/:meetingId" element={<div>로그인 페이지를 넣어주세요</div>} />
         <Route path="/host/:meetingId" element={<div>최적의 회의시간 페이지를 넣어주세요</div>} />
         <Route path="/q-card/:meetingId" element={<div>큐카드 페이지를 넣어주세요</div>} />
         <Route path="*" element={<div>not found</div>} />
