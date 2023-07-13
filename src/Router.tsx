fdbfd6e453357854571b86774d0976de8b6b94dd
ljh0608 <m98m@catholic.ac.kr>
2023-07-14 03:21:30 +0900

    fix: 링크입장페이지 url변경

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,6 @@
 import ComponentTesting from 'pages/ComponentTesting';
 import CreateMetting from 'pages/createMeeting/CreateMetting';
+import SelectSchedule from 'pages/selectSchdule/SelectSchedule';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const Router = () => {
@@ -13,7 +14,7 @@ const Router = () => {
         <Route path="/schedule/:meetingId" element={<div>가능 시간 입력페이지를 넣어주세요</div>} />
         <Route path="/priority/:meetingId" element={<div>우선순위 선정페이지를 넣어주세요</div>} />
         <Route path="/schedule-complete/:meetingId" element={<div>일정 입력 완료 페이지</div>} />
-        <Route path="/:meetingId" element={<div>링크입장페이지</div>} />
+        <Route path="/meet/:meetingId" element={<div>링크입장페이지</div>} />
         <Route path="/login/:meetingId" element={<div>로그인 페이지를 넣어주세요</div>} />
         <Route path="/host/:meetingId" element={<div>최적의 회의시간 페이지를 넣어주세요</div>} />
         <Route path="/q-card/:meetingId" element={<div>큐카드 페이지를 넣어주세요</div>} />
