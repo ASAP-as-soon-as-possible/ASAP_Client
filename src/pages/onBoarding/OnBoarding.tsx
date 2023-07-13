c83f56b1a055d46a894e5b083bceb1be915f2ebc
Chanwoo-Jeong <lordroad38@naver.com>
2023-07-14 00:58:20 +0900

    refactor: Onboarding refactoring

--- src/pages/onBoarding/OnBoarding.tsx
+++ src/pages/onBoarding/OnBoarding.tsx
@@ -1,83 +1,82 @@
+import Button from 'components/atomComponents/Button';
 import Text from 'components/atomComponents/Text';
+import { CardIc, InsertIc, MakeIc, PointIc } from 'components/Icon/icon';
 import Header from 'components/moleculesComponents/Header';
 import styled from 'styled-components/macro';
-import { theme } from 'styles/theme';
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import Explain from './components/Explain';
+
+const slides = [
+  {
+    icon: <MakeIc />,
+    main: '간단한 회의방 생성',
+    sub1: '1분만에 회의방을 만들고',
+    sub2: '회의정보와 함께 전달하고 싶은 내용을 추가해보세요',
+  },
+  {
+    icon: <InsertIc />,
+    main: '시간대 우선순위 입력',
+    sub1: '클릭 한번으로 선호하는 회의 시간의',
+    sub2: '우선순위를 입력하세요',
+  },
+  {
+    icon: <PointIc />,
+    main: '최적의 시간대 도출',
+    sub1: '모든 팀원이 만족할 만한',
+    sub2: '한계의 시간대를 안내해줄게요',
+  },
+  {
+    icon: <CardIc />,
+    main: '회의정보 큐카드 제공',
+    sub1: '한눈에 회의정보를 확인하고',
+    sub2: '쉽게 공유해보세요',
+  },
+];
 
 function OnBoarding() {
   return (
-    <OnboardingWrapper>
-      <Header position={'onBoarding'} />
-      <SwiperSection>
-        <SwiperContext>
-          <Swiper
-            spaceBetween={30}
-            centeredSlides={true}
-            autoplay={{
-              delay: 2000,
-              disableOnInteraction: false,
-            }}
-            pagination={{
-              clickable: true,
-            }}
-            navigation={true}
-            modules={[Autoplay, Pagination, Navigation]}
-            className="mySwiper"
-          >
-            <SwiperSlide>
-              <Text font={'body1'} color={`${theme.colors.white}`}>
-                Slide 1
-              </Text>
-            </SwiperSlide>
-            <SwiperSlide>
-              <Text font={'body1'} color={`${theme.colors.white}`}>
-                Slide 2
-              </Text>
-            </SwiperSlide>
-            <SwiperSlide>
-              <Text font={'body1'} color={`${theme.colors.white}`}>
-                Slide 3
-              </Text>
-            </SwiperSlide>
-            <SwiperSlide>
-              <Text font={'body1'} color={`${theme.colors.white}`}>
-                Slide 4
-              </Text>
-            </SwiperSlide>
-            <SwiperSlide>
-              <Text font={'body1'} color={`${theme.colors.white}`}>
-                Slide 5
-              </Text>
-            </SwiperSlide>
-            <SwiperSlide>
-              <Text font={'body1'} color={`${theme.colors.white}`}>
-                Slide 6
-              </Text>
-            </SwiperSlide>
-            <SwiperSlide>
-              <Text font={'body1'} color={`${theme.colors.white}`}>
-                Slide 7
-              </Text>
-            </SwiperSlide>
-            <SwiperSlide>
-              <Text font={'body1'} color={`${theme.colors.white}`}>
-                Slide 8
-              </Text>
-            </SwiperSlide>
-            <SwiperSlide>
-              <Text font={'body1'} color={`${theme.colors.white}`}>
-                Slide 9
-              </Text>
-            </SwiperSlide>
-          </Swiper>
-        </SwiperContext>
-      </SwiperSection>
-    </OnboardingWrapper>
+    <>
+      <OnboardingWrapper>
+        <Header position={'onBoarding'} />
+        <SwiperSection>
+          <SwiperContext>
+            <Swiper
+              spaceBetween={30}
+              centeredSlides={true}
+              autoplay={{
+                delay: 2000,
+                disableOnInteraction: false,
+              }}
+              pagination={{
+                clickable: true,
+              }}
+              navigation={true}
+              modules={[Autoplay, Pagination, Navigation]}
+              className="mySwiper"
+            >
+              {slides.map((slide, index) => (
+                <StyledSwiperSlide key={index}>
+                  <SvgContainer>{slide.icon}</SvgContainer>
+                  <ExplainContainer>
+                    <Explain main={slide.main} sub1={slide.sub1} sub2={slide.sub2} />
+                  </ExplainContainer>
+                </StyledSwiperSlide>
+              ))}
+            </Swiper>
+          </SwiperContext>
+        </SwiperSection>
+        <ButtonSection>
+          <Button typeState={'primaryActive'}>
+            <Text font={'button2'}>약속 생성하기</Text>
+          </Button>
+        </ButtonSection>
+      </OnboardingWrapper>
+    </>
   );
 }
 
@@ -88,16 +87,29 @@ const OnboardingWrapper = styled.div`
 `;
 
 const SwiperContext = styled.div`
-  /* border: 2px solid ${theme.colors.white}; */
   width: 100%;
   height: 50rem;
 `;
 
-const SwiperSection = styled.section`
-margin-top:3rem;
+const SwiperSection = styled.section``;
+
+const StyledSwiperSlide = styled(SwiperSlide)`
+  display: flex;
+  flex-direction: column;
 `;
 
-const Img = styled.img`
-  width: 100%;
-  height: 20rem;
+const SvgContainer = styled.section`
+  svg {
+    width: 25rem;
+    height: 25rem;
+  }
+`;
+
+const ExplainContainer = styled.section`
+  margin: 5rem 0;
+`;
+
+const ButtonSection = styled.section`
+  position: fixed;
+  bottom: 1.2rem;
 `;
