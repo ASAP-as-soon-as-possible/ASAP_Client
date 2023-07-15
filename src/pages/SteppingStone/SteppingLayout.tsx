ef74ce325c885be486293f0563c83339bfbc95ce
Chanwoo-Jeong <lordroad38@naver.com>
2023-07-16 02:46:11 +0900

    refactor:  SteppingLayout refactoring

--- src/pages/SteppingStone/SteppingLayout.tsx
+++ src/pages/SteppingStone/SteppingLayout.tsx
@@ -1,25 +1,20 @@
-import stepingCheck from 'assets/images/steppingCheck.png';
-import Button from 'components/atomComponents/Button';
-import Text from 'components/atomComponents/Text';
 import Header from 'components/moleculesComponents/Header';
 import styled from 'styled-components/macro';
-import { theme } from 'styles/theme';
-import SteppingBody from './components/SteppingBody';
 
-function SteppingLayout() {
+import SteppingBody from './components/SteppingBody';
+import SteppingBtnSection from './components/SteppingBtnSection';
+
+interface SteppingProps {
+  steppingType: string;
+}
+
+function SteppingLayout({ steppingType }: SteppingProps) {
   return (
     <>
       <SteppingWrapper>
         <Header position={'stepping'} />
-        <SteppingBody />
-        <StyledBtnSection>
-          <Button typeState={'halfTertiaryActive'}>
-            <Text font={'button2'}>링크 복사하기</Text>
-          </Button>
-          <Button typeState={'halfPrimaryActive'}>
-            <Text font={'button2'}>나의 가능시간 입력</Text>
-          </Button>
-        </StyledBtnSection>
+        <SteppingBody steppingType={steppingType} />
+        <SteppingBtnSection steppingType={steppingType} />
       </SteppingWrapper>
     </>
   );
@@ -32,16 +27,5 @@ const SteppingWrapper = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  width: 100%;
-`;
-
-const StyledBtnSection = styled.section`
-  display: flex;
-  position: fixed;
-  bottom: 1.2rem;
-  flex-direction: row;
-  gap: 1.4rem;
-  justify-content: center;
-  border-radius: 50%;
   width: 100%;
 `;
