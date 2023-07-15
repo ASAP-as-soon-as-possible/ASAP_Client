d63f99d7f3a35ed0eb243114aa65252214f4ca2b
Chanwoo-Jeong <lordroad38@naver.com>
2023-07-15 19:17:59 +0900

    refactor: SteppingBody refactoring

--- src/pages/SteppingStone/SteppingLayout.tsx
+++ src/pages/SteppingStone/SteppingLayout.tsx
@@ -4,23 +4,14 @@ import Text from 'components/atomComponents/Text';
 import Header from 'components/moleculesComponents/Header';
 import styled from 'styled-components/macro';
 import { theme } from 'styles/theme';
+import SteppingBody from './components/SteppingBody';
 
 function SteppingLayout() {
   return (
     <>
       <SteppingWrapper>
         <Header position={'stepping'} />
-        <ImageSection>
-          <img src={stepingCheck} alt="png" />
-        </ImageSection>
-        <SteppingMentSection>
-          <Text font={'head1'} color={`${theme.colors.white}`}>
-            회의 생성 완료!
-          </Text>
-          <Text font={'body4'} color={`${theme.colors.grey4}`}>
-            이제 가능한 시간을 입력하러 가볼까요?
-          </Text>
-        </SteppingMentSection>
+        <SteppingBody />
         <StyledBtnSection>
           <Button typeState={'halfTertiaryActive'}>
             <Text font={'button2'}>링크 복사하기</Text>
@@ -44,20 +35,6 @@ const SteppingWrapper = styled.div`
   width: 100%;
 `;
 
-const ImageSection = styled.section`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-top: 12rem;
-  img {
-    display: flex;
-    flex-direction: center;
-    align-items: center;
-    width: 21.3rem;
-    height: 19.9rem;
-  }
-`;
-
 const StyledBtnSection = styled.section`
   display: flex;
   position: fixed;
@@ -67,13 +44,4 @@ const StyledBtnSection = styled.section`
   justify-content: center;
   border-radius: 50%;
   width: 100%;
-`;
-
-const SteppingMentSection = styled.section`
-  display: flex;
-  flex-direction: column;
-  gap: 2.5rem;
-  align-items: center;
-  justify-content: center;
-  margin-top: 2.5rem;
 `;
