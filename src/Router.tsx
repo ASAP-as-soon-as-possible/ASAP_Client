0842b4f2a5d52467c3d6aa42f0bff1aeab188efc
Chanwoo-Jeong <lordroad38@naver.com>
2023-07-16 04:17:11 +0900

    feat: Router.tsx 컴포넌트 연결

--- src/Router.tsx
+++ src/Router.tsx
@@ -17,8 +17,11 @@ const Router = () => {
         <Route path="/meet/complete" element={<SteppingLayout steppingType={'meetComplete'} />} />
         <Route path="/schedule/:meetingId" element={<div>가능 시간 입력페이지를 넣어주세요</div>} />
         <Route path="/priority/:meetingId" element={<div>우선순위 선정페이지를 넣어주세요</div>} />
-        <Route path="/schedule-complete/:meetingId" element={<div>일정 입력 완료 페이지</div>} />
-        <Route path="/meet/:meetingId" element={<div>링크입장페이지</div>} />
+        <Route
+          path="/schedule-complete/:meetingId"
+          element={<SteppingLayout steppingType={'hostScheduleComplete'} />}
+        />
+        <Route path="/meet/:meetingId" element={<SteppingLayout steppingType={'meetEntrance'} />} />
         <Route path="/login/:meetingId" element={<div>로그인 페이지를 넣어주세요</div>} />
         <Route path="/host/:meetingId" element={<div>최적의 회의시간 페이지를 넣어주세요</div>} />
         <Route path="/q-card/:meetingId" element={<CueCard />} />
