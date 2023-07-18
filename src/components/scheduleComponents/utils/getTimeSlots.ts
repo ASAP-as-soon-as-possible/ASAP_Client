33b5638c36c56d8009a2cd9b84178fccd03068cc
simeunseo <rkdcjfehowl@naver.com>
2023-07-19 04:22:45 +0900

    feat: 종료 시간이 시작 시간보다 이전일시 alert 및 시간표에 띄우지 않음

--- src/components/scheduleComponents/utils/getTimeSlots.ts
+++ src/components/scheduleComponents/utils/getTimeSlots.ts
@@ -3,6 +3,10 @@ import { PreferTime } from '../types/AvailableScheduleType';
 const getTimeSlots = (preferTimes: PreferTime[]) => {
   const timeSlots = preferTimes.flatMap((obj) => {
     const { startTime, endTime } = obj;
+    if (parseInt(startTime) >= parseInt(endTime)) {
+      alert('시작 시간은 종료 시간 이후로 설정해주세요!');
+      return;
+    }
     if (startTime === '' || endTime === '') return;
     const slots = [];
     let currentTime = startTime;
