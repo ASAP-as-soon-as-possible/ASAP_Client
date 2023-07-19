ddd0a6b1635e40338b5bc9f27de457bad6d3ad7f
ljh0608 <m98m@catholic.ac.kr>
2023-07-19 10:44:19 +0900

    design: priority drop down proto

---
+++ src/components/scheduleComponents/components/PriorityDropdown.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Text from 'components/atomComponents/Text';
+import styled from 'styled-components/macro';
+
+function PriorityDropdown() {
+  return <div>PriorityDropdown</div>;
+}
+
+export default PriorityDropdown;
