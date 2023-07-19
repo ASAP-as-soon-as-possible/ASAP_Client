22efe13b9be70f3b38c43b0f7b2579924692f7c3
Chanwoo-Jeong <lordroad38@naver.com>
2023-07-19 16:10:11 +0900

    feat: 확정된 회의인지 아닌지 판단하는 함수 useEffect 처리

--- src/pages/createMeeting/CreateMeeting.tsx
+++ src/pages/createMeeting/CreateMeeting.tsx
@@ -9,24 +9,38 @@ import { funnelStep } from './data/meetingInfoData';
 import { MeetingInfo } from './types/useFunnelInterface';
 
 const initialMeetingInfo: MeetingInfo = {
-  title: '',
-  availableDates: [''],
+  // title: '',
+  // availableDates: [''],
+  // preferTimes: [
+  //   {
+  //     startTime: '',
+  //     endTime: '',
+  //   },
+  // ],
+  // place: '',
+  // placeDetail: '',
+  // duration: '',
+  // name: '',
+  // password: '',
+  // additionalInfo: '',
+  title: 'ASAP 회의',
+  availableDates: ['2023/07/06/MON'],
   preferTimes: [
     {
-      startTime: '',
-      endTime: '',
+      startTime: '06:00',
+      endTime: '12:00',
     },
   ],
-  place: '',
-  placeDetail: '',
-  duration: '',
-  name: '',
-  password: '',
-  additionalInfo: '',
+  place: 'ONLINE',
+  placeDetail: 'zoom',
+  duration: 'HALF',
+  name: '서지원',
+  password: '0702',
+  additionalInfo: '추가 공지사항',
 };
 
 function CreateMeeting() {
-  const [step, setStep] = useState(0);
+  const [step, setStep] = useState(6);
   const [meetingInfo, setMeetingInfo] = useState(initialMeetingInfo);
   const currentStep = funnelStep[step];
 
--- src/utils/apis/axios.ts
+++ src/utils/apis/axios.ts
@@ -2,13 +2,13 @@ import axios, { AxiosInstance } from 'axios';
 
 export const client:AxiosInstance = axios.create({
     baseURL: import.meta.env.VITE_APP_IP,
-    headers: {'Content-Type': 'application/json', }
+    headers: {'Content-Type': 'application/json' }
 })
 
 
 export const authClient:AxiosInstance = axios.create({
     baseURL: import.meta.env.VITE_APP_IP,
-    headers: {'Content-Type': 'application/json', }
+    headers: {'Content-Type': 'application/json' }
 })
 
 authClient.interceptors.request.use(
