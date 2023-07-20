12472f53331c3a3b35f485a0cd3f5c555a2dce66
ljh0608 <m98m@catholic.ac.kr>
2023-07-21 02:25:57 +0900

    feat: api type지정

--- src/types/createAvailableSchduleType.ts
+++ src/types/createAvailableSchduleType.ts
@@ -1,15 +1,37 @@
-export interface AvailableSchduleRequestType {
-  id: number;
+export interface HostAvailableSchduleRequestType {
+  id: string;
   month: string;
+  day: string;
   dayOfWeek: string;
   startTime: string;
   endTime: string;
   priority: number;
 }
 
-export interface AvailableScheduleResponseType {
+export interface HostAvailableScheduleResponseType {
   data: {
     url: string;
     accessToken: string;
+  };
+}
+
+export interface UserAvailableScheduleRequestType {
+  name: string;
+  availableTimes: [
+    {
+      id: string;
+      month: string;
+      day: string;
+      dayOfWeek: string;
+      startTime: string;
+      endTime: string;
+      priority: number;
+    }
+  ];
+}
+
+export interface UserAvailableScheduleResponseType {
+  data: {
+    role: string;
   };
 }
