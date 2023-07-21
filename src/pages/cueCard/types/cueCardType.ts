049c7afb624fd1c0c99fda88556ad4a688c65294
Chanwoo-Jeong <lordroad38@naver.com>
2023-07-22 03:36:09 +0900

    fix: null ===  ts 오류

--- src/pages/cueCard/types/cueCardType.ts
+++ src/pages/cueCard/types/cueCardType.ts
@@ -1,7 +1,7 @@
 export interface CueCardDataType {
   title: string;
   place: 'ONLINE' | 'OFFLINE' | 'UNDEFINED';
-  placeDetail: string | null;
+  placeDetail: string;
   month: string;
   day: string;
   dayOfWeek: string;
@@ -9,7 +9,7 @@ export interface CueCardDataType {
   endTime: string;
   hostName: string;
   userNames: string[];
-  additionalInfo: string | null;
+  additionalInfo: string;
 }
 
 export interface CueCardResponse {
