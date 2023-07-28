de0056529424b955cc5f8380172862690bf24eea
simeunseo <rkdcjfehowl@naver.com>
2023-07-28 15:32:11 +0900

    feat: Navigation 컴포넌트 생ㅅ어

---
+++ src/components/moleculesComponents/Navigation.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import Text from 'components/atomComponents/Text';
+import { Link } from 'react-router-dom';
+import { theme } from 'styles/theme';
+
+interface NavigationOption {
+  title: string;
+  url: string;
+}
+
+interface NavigationProps {
+  navigationOptions: NavigationOption[];
+}
+
+function Navigation({ navigationOptions }: NavigationProps) {
+  return (
+    <>
+      {navigationOptions.map((option, i) => (
+        <Link to={option.url} key={i + option.title}>
+          <Text font={'title2'} color={theme.colors.white}>
+            {option.title}
+          </Text>
+        </Link>
+      ))}
+    </>
+  );
+}
+
+export default Navigation;
