37726e94166807d11fe45b2a1dffdfca23d0e1ca
DongHeonLee <eastlaw80@naver.com>
2023-10-15 15:27:12 +0900

    fix: 오타수정

--- src/components/atomComponents/PasswordInput.tsx
+++ src/components/atomComponents/PasswordInput.tsx
@@ -27,7 +27,7 @@ function PasswordInput({ value, placeholder, passWordOnChange, page }: ValueProp
           onChange={passWordOnChange}
           $iserror={value.length < 4}
           type={inputType ? `number` : undefined}
-          inputmode="numeric"
+          inputMode="numeric"
         />
         <IconContainer onClick={changePasswordType}>
           {inputType ? <PasswordOpenEyeIc /> : <PasswordEyeIc />}
