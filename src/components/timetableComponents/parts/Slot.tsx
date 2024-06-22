091854a01b5cf0a52874f485133dffcbdc3a0744
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-06-22 17:45:45 +0900

    refactor: Slot 컴포넌트 스타일링 조건부로직 개선

--- src/components/timetableComponents/parts/Slot.tsx
+++ src/components/timetableComponents/parts/Slot.tsx
@@ -1,6 +1,5 @@
+import useSlotSeletion from 'pages/selectSchedule/selectTimeSlot/hooks/useSlotSelection';
 import styled from 'styled-components';
-
-import useSlotSeletion from '../hooks/useSlotSelection';
 
 interface SlotProps {
   slot: string;
@@ -11,32 +10,30 @@ function Slot({ slot, selectedEntryId }: SlotProps) {
   const { startSlot, onClickSlot } = useSlotSeletion();
 
   const borderStyle = slot.endsWith(':30') ? 'dashed' : 'solid';
-  const styledSlotProps = {
-    $borderStyle: borderStyle,
-    $isSelected: selectedEntryId !== undefined,
-    onClick: () => onClickSlot(slot, selectedEntryId),
-  };
+  const isSelected = selectedEntryId !== undefined;
+  const isStartSlot = slot === startSlot;
 
-  if (slot === startSlot) {
-    return <SelectingSlot {...styledSlotProps} />;
-  } else {
-    return <DefaultSlot {...styledSlotProps} />;
-  }
+  return (
+    <DefaultSlot
+      $borderStyle={borderStyle}
+      $isSelected={isSelected}
+      $isStartSlot={isStartSlot}
+      onClick={() => onClickSlot(slot, selectedEntryId)}
+    />
+  );
 }
 
 export default Slot;
 
-const DefaultSlot = styled.div<{ $borderStyle: string; $isSelected: boolean }>`
-  border-top: 1px ${({ $borderStyle }) => $borderStyle} ${({ theme }) => theme.colors.grey7};
-  background-color: ${({ $isSelected, theme }) =>
-    $isSelected ? theme.colors.main1 : 'transparent'};
-  cursor: pointer;
-  width: 4.4rem;
-  height: 2.2rem;
-`;
-
-const SelectingSlot = styled.div<{ $borderStyle: string; $isSelected: boolean }>`
-  border: 1px dashed ${({ theme }) => theme.colors.main5};
+const DefaultSlot = styled.div<{
+  $borderStyle: string;
+  $isSelected: boolean;
+  $isStartSlot: boolean;
+}>`
+  ${({ $isStartSlot, $borderStyle, theme }) =>
+    $isStartSlot
+      ? `border: 1px dashed ${theme.colors.main5};`
+      : `border-top: 1px ${$borderStyle} ${theme.colors.grey7};`}
   background-color: ${({ $isSelected, theme }) =>
     $isSelected ? theme.colors.main1 : 'transparent'};
   cursor: pointer;
