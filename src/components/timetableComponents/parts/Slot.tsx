89f2b92a9efeb62a5b950de3955dd572691adeed
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-06-22 18:16:37 +0900

    refactor: Slot에서 의존성 분리

--- src/components/timetableComponents/parts/Slot.tsx
+++ src/components/timetableComponents/parts/Slot.tsx
@@ -1,41 +1,25 @@
-import useSlotSeletion from 'pages/selectSchedule/selectTimeSlot/hooks/useSlotSelection';
 import styled from 'styled-components';
 
 interface SlotProps {
-  slot: string;
-  selectedEntryId?: number;
+  slotId: string;
+  slotStyle?: string;
+  onClick?: () => void;
 }
 
-function Slot({ slot, selectedEntryId }: SlotProps) {
-  const { startSlot, onClickSlot } = useSlotSeletion();
+function Slot({ slotId, slotStyle, onClick }: SlotProps) {
+  const borderStyle = slotId.endsWith(':30') ? 'dashed' : 'solid';
 
-  const borderStyle = slot.endsWith(':30') ? 'dashed' : 'solid';
-  const isSelected = selectedEntryId !== undefined;
-  const isStartSlot = slot === startSlot;
-
-  return (
-    <DefaultSlot
-      $borderStyle={borderStyle}
-      $isSelected={isSelected}
-      $isStartSlot={isStartSlot}
-      onClick={() => onClickSlot(slot, selectedEntryId)}
-    />
-  );
+  return <DefaultSlot $borderStyle={borderStyle} $slotStyle={slotStyle} onClick={onClick} />;
 }
 
 export default Slot;
 
 const DefaultSlot = styled.div<{
   $borderStyle: string;
-  $isSelected: boolean;
-  $isStartSlot: boolean;
+  $slotStyle?: string;
 }>`
-  ${({ $isStartSlot, $borderStyle, theme }) =>
-    $isStartSlot
-      ? `border: 1px dashed ${theme.colors.main5};`
-      : `border-top: 1px ${$borderStyle} ${theme.colors.grey7};`}
-  background-color: ${({ $isSelected, theme }) =>
-    $isSelected ? theme.colors.main1 : 'transparent'};
+  ${({ $slotStyle }) => $slotStyle};
+  border-top-style: ${({ $borderStyle }) => $borderStyle};
   cursor: pointer;
   width: 4.4rem;
   height: 2.2rem;
--- src/pages/selectSchedule/selectTimeSlot/components/SelectionSlots.tsx
+++ src/pages/selectSchedule/selectTimeSlot/components/SelectionSlots.tsx
@@ -1,5 +1,8 @@
+import { theme } from 'styles/theme';
+
 import { useTimetableContext } from '../../../../components/timetableComponents/context';
 import Slot from '../../../../components/timetableComponents/parts/Slot';
+import useSlotSeletion from '../hooks/useSlotSelection';
 
 interface SelectionSlotsProps {
   date: string;
@@ -12,6 +15,23 @@ function SelectionSlots({ date, timeSlots }: SelectionSlotsProps) {
     ([, slot]) => slot.date === date,
   );
 
+  const { startSlot, onClickSlot } = useSlotSeletion();
+
+  const getTimeSlotStyle = (slotId: string, selectedEntryId?: number) => {
+    const isStartSlot = slotId === startSlot;
+    const isSelectedSlot = selectedEntryId !== undefined;
+
+    return `
+      ${
+        isStartSlot
+          ? `border: 1px dashed ${theme.colors.main5}`
+          : `border-top: 1px solid ${theme.colors.grey7}`
+      };
+      ${
+        isSelectedSlot ? `background-color: ${theme.colors.main1}` : `background-color: transparent`
+      };
+    `;
+  };
   return (
     <>
       {timeSlots.map((timeSlot) => {
@@ -21,11 +41,15 @@ function SelectionSlots({ date, timeSlots }: SelectionSlotsProps) {
 
         const selectedEntryId = belongingEntry ? parseInt(belongingEntry[0]) : undefined;
 
+        const slotId = `${date}/${timeSlot}`;
+
+        const slotStyle = getTimeSlotStyle(slotId, selectedEntryId);
         return (
           <Slot
-            key={`${date}/${timeSlot}`}
-            slot={`${date}/${timeSlot}`}
-            selectedEntryId={selectedEntryId}
+            key={slotId}
+            slotId={slotId}
+            slotStyle={slotStyle}
+            onClick={() => onClickSlot(slotId, selectedEntryId)}
           />
         );
       })}
