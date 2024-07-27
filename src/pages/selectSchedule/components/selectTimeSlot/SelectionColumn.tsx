4307dc796d7fbe4b9a625a5b1753959ae18d87fd
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-07-27 22:59:08 +0900

    feat: 맨 첫번째 Slot은 그라디언트 적용

--- src/pages/selectSchedule/components/selectTimeSlot/SelectionColumn.tsx
+++ src/pages/selectSchedule/components/selectTimeSlot/SelectionColumn.tsx
@@ -18,23 +18,30 @@ function SelectionColumn({ date, timeSlots }: ColumnStructure) {
   const getTimeSlotStyle = (slotId: string, selectedEntryId?: number) => {
     const isStartSlot = slotId === startSlot;
     const isSelectedSlot = selectedEntryId !== undefined;
+    const isFirstSlot =
+      selectedEntryId !== undefined &&
+      selectedSlots[selectedEntryId].startSlot === slotId.split('/')[3];
 
     /**
      * 가능시간 입력 시간표 스타일링
      * 1. border-top: 30분 단위는 dashed, 1시간 단위는 solid
      * 2. border: 탭투탭 시 startSlot에 dashed
-     * 3. background-color: 탭투탭으로 선택된 시간이면 main1, 그 외는 transparent;
+     * 3. background-color: 선택된 시간이면서 맨 첫번째 Slot이라면 그라디언트, 그 외는 main1, 선택된 시간이 아니면 transparent;
      */
     const borderStyle = slotId.endsWith(':30') ? 'dashed' : 'solid';
     const border = isStartSlot && `1px dashed ${theme.colors.main5}`;
     const borderTop = `1px ${borderStyle} ${theme.colors.grey7}`;
-    const backgroundColor = isSelectedSlot ? theme.colors.main1 : 'transparent';
+    const background = isSelectedSlot
+      ? isFirstSlot
+        ? 'linear-gradient(180deg, #496AFF 0%, #3253FF 75%)'
+        : theme.colors.main1
+      : 'transparent';
 
     return `
       cursor:pointer;
       border-top: ${borderTop};
       ${isStartSlot && `border: ${border}`};
-      background-color: ${backgroundColor};
+      background: ${background};
     `;
   };
 
