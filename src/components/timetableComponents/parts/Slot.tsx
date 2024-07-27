6c15e07e7408c75bbf10a9f1c76f1ff55243a517
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-07-27 21:11:31 +0900

    fix: slot의 스타일링에 대한 로직을 Column 컴포넌트로 이동

--- src/components/timetableComponents/parts/Slot.tsx
+++ src/components/timetableComponents/parts/Slot.tsx
@@ -3,14 +3,13 @@ import { ReactNode } from 'react';
 import styled from 'styled-components';
 
 interface SlotProps {
-  slotId: string;
   customSlotStyle?: string;
   slotUnit: 'HALF' | 'HOUR';
   onClick?: () => void;
   children?: ReactNode;
 }
 
-function Slot({ slotId, customSlotStyle, slotUnit, onClick, children }: SlotProps) {
+function Slot({ customSlotStyle, slotUnit, onClick, children }: SlotProps) {
   const defaultSlotStyle = `
     width: 4.4rem;
     height: ${slotUnit === 'HALF' ? '2.2rem' : '1.2rem'};
@@ -18,16 +17,9 @@ function Slot({ slotId, customSlotStyle, slotUnit, onClick, children }: SlotProp
     justify-content: center;
   `;
 
-  const borderTopStyle = slotId.endsWith(':30')
-    ? slotUnit === 'HALF'
-      ? 'dashed'
-      : 'none'
-    : 'solid';
-
   return (
     <SlotWrapper
       $defaultSlotStyle={defaultSlotStyle}
-      $borderTopStyle={borderTopStyle}
       $customSlotStyle={customSlotStyle}
       onClick={onClick}
     >
@@ -40,11 +32,8 @@ export default Slot;
 
 const SlotWrapper = styled.div<{
   $defaultSlotStyle: string;
-  $borderTopStyle: string;
   $customSlotStyle?: string;
 }>`
-  border-top: 1px solid ${({ theme }) => theme.colors.grey7};
-  border-top-style: ${({ $borderTopStyle }) => $borderTopStyle};
   ${({ $defaultSlotStyle }) => $defaultSlotStyle};
   ${({ $customSlotStyle }) => $customSlotStyle};
 `;
