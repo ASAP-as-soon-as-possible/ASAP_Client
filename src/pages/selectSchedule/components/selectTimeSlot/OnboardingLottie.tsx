bd09cea76ab8db58641c2b43a02e248511b426a1
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-08-04 16:44:13 +0900

    feat: useModalState 구현

--- src/pages/selectSchedule/components/selectTimeSlot/OnboardingLottie.tsx
+++ src/pages/selectSchedule/components/selectTimeSlot/OnboardingLottie.tsx
@@ -1,8 +1,20 @@
+import Lottie from 'react-lottie';
+import taptotapOnboarding from 'src/assets/lotties/taptotap_onboarding.json';
 
 function OnboardingLottie() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: taptotapOnboarding,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
   return (
-    <div><Lottie</div>
-  )
+    <div>
+      <Lottie options={defaultOptions} />
+    </div>
+  );
 }
 
-export default OnboardingLottie
+export default OnboardingLottie;
