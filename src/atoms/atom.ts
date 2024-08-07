0317bb8a67e622586020e563ba2a822a7887b354
ljh0608 <m98m@naver.com>
2024-08-07 23:09:58 +0900

    충돌 해결

--- src/atoms/atom.ts
+++ src/atoms/atom.ts
@@ -1,21 +1,10 @@
-import { PreferTime } from 'components/legacy/scheduleComponents/types/AvailableScheduleType';
-import { DateStates, ScheduleStates } from 'pages/legacy/selectSchedule/types/Schedule';
+import { ScheduleStates } from 'pages/legacy/selectSchedule/types/Schedule';
 import { atom } from 'recoil';
 import { recoilPersist } from 'recoil-persist';
 
 export const methodStateAtom = atom<boolean>({
   key: 'methodStateAtom',
   default: false,
-});
-
-export const availableDatesAtom = atom<DateStates[]>({
-  key: 'availableDatesAtom',
-  default: [],
-});
-
-export const preferTimesAtom = atom<PreferTime[]>({
-  key: 'preferTimesAtom',
-  default: [],
 });
 
 export const scheduleAtom = atom<ScheduleStates[]>({
