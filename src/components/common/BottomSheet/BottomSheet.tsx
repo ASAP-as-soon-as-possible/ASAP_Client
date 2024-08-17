ab6143900070d4aff91c08ca38e3795110be7ab7
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-08-18 01:35:29 +0900

    feat: 회의 생성 전 확인 바텀시트 구현

--- src/components/common/BottomSheet/BottomSheet.tsx
+++ src/components/common/BottomSheet/BottomSheet.tsx
@@ -32,7 +32,7 @@ const BottomSheetModal = styled.div<{ $isModalOpen: boolean }>`
   border-top-right-radius: 1.2rem;
   background-color: ${({ theme }) => theme.colors.grey8};
 
-  padding: 2.8rem 2rem 4rem;
+  padding: 4.4rem 2rem 4rem;
   width: 37.5rem;
 
   & button {
